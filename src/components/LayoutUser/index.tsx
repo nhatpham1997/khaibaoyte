bbe037f461a777535041805a8569c232fc055bbd
Nguyen-Van-Duy <nam1561999@gmail.com>
2022-01-26 20:40:36 +0700

    add handle search Account

--- src/components/LayoutUser/index.tsx
+++ src/components/LayoutUser/index.tsx
@@ -1,29 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './Header'
 import Content from './Content'
 import Footer from './Footer'
 import Nav from './Nav'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { Box } from '@mui/material'
 
 const LayoutUser = () => {
   const [titleHeader, setTitleHeader] = useState(
     localStorage.getItem('title') || 'Khai báo di chuyển'
   )
-
+  const [loading, setLoading] = useState(true)
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (localStorage.getItem('userId')) {
+      setLoading(false)
+    } else {
+      navigate('/')
+    }
+  }, [])
   return (
-    <div className="app">
-      {/* <Box sx={{ display: { xs: 'none', lg: 'unset' } }}>
-      </Box> */}
-      <Nav setTitleHeader={setTitleHeader} />
-      <div className="wrap">
-        <Header titleHeader={titleHeader} />
-        <Content>
-          <Outlet />
-        </Content>
-        <Footer />
-      </div>
-    </div>
+    <>
+      {!loading && (
+        <div className="app">
+          <Nav setTitleHeader={setTitleHeader} />
+          <div className="wrap">
+            <Header titleHeader={titleHeader} />
+            <Content>
+              <Outlet />
+            </Content>
+            <Footer />
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
