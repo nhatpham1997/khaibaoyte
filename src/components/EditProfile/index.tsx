6c293fe3739b797eb65508132f8c5beef857eb4b
phuhoang97 <59466061+phuhoang97@users.noreply.github.com>
2022-01-26 08:42:46 +0700

    registerform and editProfile

--- src/components/EditProfile/index.tsx
+++ src/components/EditProfile/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import { useEffect, useState } from 'react'
 import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 import CssBaseline from '@mui/material/CssBaseline'
@@ -13,20 +13,94 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Typography from '@mui/material/Typography'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { NavLink } from 'react-router-dom'
+import axios from 'axios'
+import MenuItem from '@mui/material/MenuItem'
 
 const theme = createTheme()
 
 export default function EditProfile() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const data = new FormData(event.currentTarget)
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault()
+  //   const data = new FormData(event.currentTarget)
+  //   // eslint-disable-next-line no-console
+  //   console.log({
+  //     email: data.get('email'),
+  //     password: data.get('password'),
+  //   })
+  // }
+  const [provinceResidences, setProvinceResidences] = useState<any[]>([])
+  const [provinces, setProvinces] = useState<any[]>([])
+  const [districtResidences, setDistrictResidences] = useState<any[]>([])
+  const [wardResidences, setWardResidences] = useState<any[]>([])
+
+  const [user, setUser] = useState<any>()
+  const [data, setData] = useState({
+    fullName: '',
+    yearOfBirth: '',
+    citizenIdentification: '',
+    gender: '',
+    province: '',
+    provinceName: '',
+    district: '',
+    ward: '',
+    specificAddress: '',
+    phone: '',
+    createdDate: '',
+  })
+
+  useEffect(() => {
+    fetch('https://provinces.open-api.vn/api/?depth=3')
+      .then((res) => res.json())
+      .then((data) => {
+        setProvinceResidences(data)
+        setProvinces(data)
+      })
+  }, [])
+
+  const handleSubmit = () => {
+    axios.put('https://dbkhaibaoyte.herokuapp.com/user/1/', data).then((res) => {
+      console.log('res', res)
     })
   }
-
+  // Hàm xử lý chọn tỉnh/thành phố cư trú
+  function handleChangeProvinceResidence(e: React.ChangeEvent<HTMLInputElement>) {
+    const codeProvinceResidence = e.target.value
+    provinceResidences.forEach((provinceResidence) => {
+      if (provinceResidence.code === codeProvinceResidence) {
+        setDistrictResidences(provinceResidence.districts)
+      }
+    })
+    setData((old) => {
+      return {
+        ...old,
+        province: codeProvinceResidence,
+      }
+    })
+  }
+  // Hàm xử lý chọn quận/huyện cư trú
+  function handleChangeDistrictResidence(e: React.ChangeEvent<HTMLInputElement>) {
+    const codeDistrictResidence = e.target.value
+    districtResidences.forEach((districtResidence) => {
+      if (districtResidence.code === codeDistrictResidence) {
+        setWardResidences(districtResidence.wards)
+      }
+    })
+    setData((old) => {
+      return {
+        ...old,
+        district: codeDistrictResidence,
+      }
+    })
+  }
+  // Hàm xử lý chọn phường/xã cư trú
+  function handleChangeWardResidence(e: React.ChangeEvent<HTMLInputElement>) {
+    setData((old) => {
+      return {
+        ...old,
+        ward: e.target.value,
+      }
+    })
+  }
   return (
     <Box>
       <ThemeProvider theme={theme}>
@@ -51,6 +125,11 @@ export default function EditProfile() {
               name="fullname"
               autoComplete="fullname"
               autoFocus
+              onChange={(e) =>
+                setData((old) => {
+                  return { ...old, fullName: e.target.value }
+                })
+              }
             />
             <TextField
               margin="normal"
@@ -61,6 +140,11 @@ export default function EditProfile() {
               name="yearofbirth"
               autoComplete="yearofbirth"
               autoFocus
+              onChange={(e) =>
+                setData((old) => {
+                  return { ...old, yearOfBirth: e.target.value }
+                })
+              }
             />
             <TextField
               margin="normal"
@@ -71,6 +155,11 @@ export default function EditProfile() {
               name="citizen_identification"
               autoComplete="citizen_identification"
               autoFocus
+              onChange={(e) =>
+                setData((old) => {
+                  return { ...old, citizenIdentification: e.target.value }
+                })
+              }
             />
             <TextField
               margin="normal"
@@ -81,27 +170,80 @@ export default function EditProfile() {
               name="gender"
               autoComplete="gender"
               autoFocus
+              onChange={(e) =>
+                setData((old) => {
+                  return { ...old, gender: e.target.value }
+                })
+              }
             />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="province"
-              label="Province"
-              name="province"
-              autoComplete="province"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="district"
-              label="District"
-              name="district"
-              autoComplete="district"
-              autoFocus
-            />
+            <div className="row">
+              <TextField
+                id="province-residence"
+                label="Tỉnh/Thành phố"
+                sx={{
+                  marginBottom: '1rem',
+                  marginTop: '1rem',
+                  minWidth: 'calc(calc(100%/4) - 1.5rem)',
+                }}
+                size="medium"
+                InputProps={{ style: { fontSize: '1.2rem' } }}
+                InputLabelProps={{ style: { fontSize: '1.2rem' } }}
+                required
+                select
+                value={data.province}
+                onChange={handleChangeProvinceResidence}
+              >
+                {provinceResidences.map((provinceResidence) => (
+                  <MenuItem key={provinceResidence.code} value={provinceResidence.code}>
+                    {provinceResidence.name}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                id="district-residence"
+                label="Quận/Huyện"
+                sx={{
+                  marginBottom: '1rem',
+                  marginTop: '1rem',
+                  minWidth: 'calc(calc(100%/4) - 1.5rem)',
+                }}
+                size="medium"
+                InputProps={{ style: { fontSize: '1.2rem' } }}
+                InputLabelProps={{ style: { fontSize: '1.2rem' } }}
+                required
+                select
+                value={data.district}
+                onChange={handleChangeDistrictResidence}
+              >
+                {districtResidences.map((districtResidence) => (
+                  <MenuItem key={districtResidence.code} value={districtResidence.code}>
+                    {districtResidence.name}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                id="ward-residence"
+                label="Phường/Xã"
+                sx={{
+                  marginBottom: '1rem',
+                  marginTop: '1rem',
+                  minWidth: 'calc(calc(100%/4) - 1.5rem)',
+                }}
+                size="medium"
+                InputProps={{ style: { fontSize: '1.2rem' } }}
+                InputLabelProps={{ style: { fontSize: '1.2rem' } }}
+                required
+                select
+                value={data.ward}
+                onChange={handleChangeWardResidence}
+              >
+                {wardResidences.map((wardResidence) => (
+                  <MenuItem key={wardResidence.code} value={wardResidence.code}>
+                    {wardResidence.name}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </div>
             <TextField
               margin="normal"
               required
@@ -111,6 +253,11 @@ export default function EditProfile() {
               name="ward"
               autoComplete="ward"
               autoFocus
+              onChange={(e) =>
+                setData((old) => {
+                  return { ...old, ward: e.target.value }
+                })
+              }
             />
             <TextField
               margin="normal"
@@ -121,22 +268,14 @@ export default function EditProfile() {
               name="phone"
               autoComplete="phone"
               autoFocus
+              onChange={(e) =>
+                setData((old) => {
+                  return { ...old, phone: e.target.value }
+                })
+              }
             />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="create_date"
-              label="Create_Date"
-              type="create_date"
-              id="create_date"
-              autoComplete="create_date"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+
+            <Button onClick={handleSubmit} fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
               Save
             </Button>
           </Box>
--- src/components/ForgotPassword/index.tsx
+++ src/components/ForgotPassword/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import { useEffect, useState } from 'react'
 import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 import CssBaseline from '@mui/material/CssBaseline'
@@ -13,73 +13,10 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Typography from '@mui/material/Typography'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { NavLink } from 'react-router-dom'
+import axios from 'axios'
 
 const theme = createTheme()
 
 export default function ForgotPassword() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const data = new FormData(event.currentTarget)
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    })
-  }
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-          mx: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Forgot PassWord
-        </Typography>
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="current_password"
-            label="Current Password"
-            name="current_password"
-            autoComplete="current_password"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="new_password"
-            label="New Password"
-            type="new_password"
-            id="new_password"
-            autoComplete="new_password"
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="confirm_new_password"
-            label="Confirm New Password"
-            type="confirm_new_password"
-            id="confirm_new_password"
-            autoComplete="confirm_new_password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-            Save
-          </Button>
-        </Box>
-      </Box>
-    </ThemeProvider>
-  )
+  return <div></div>
 }
